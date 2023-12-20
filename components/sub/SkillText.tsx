d30c54bf18ae5bb559980a947502b9424adcbbf6
Akim Kotovskiy <ak.kotow2020@gmail.com>
2023-12-20 03:07:18 +0300

    complete this work. its done

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -24,9 +24,9 @@ const SkillText = () => {
         </motion.div>
         <motion.div
         variants={slideInFromRight(0.5)}
-        className='cursive text-[20px] text-'
+        className='cursive text-[20px] text-gray-200 mb-10 mt-[10px] text-center'
         >
-
+          Never miss a task deadline or idea
         </motion.div>
     </div>
   )
